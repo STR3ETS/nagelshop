7117c7f4515b770d4d83670c4fdd7fdb710d24d4
Boyd Halfman <78419518+STR3ETS@users.noreply.github.com>
2025-10-25 15:18:00 +0200

    Seeder

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+// Zorg dat alle categorie-afbeeldingen in de build/manifest komen:
+import.meta.glob('../images/catogorieen-fotos/*.{webp,jpg,jpeg,png,svg}', { eager: true, as: 'url' });
+
+// (Eventueel breder)
+// import.meta.glob('../images/**/*.{webp,jpg,jpeg,png,svg}', { eager: true, as: 'url' });
+
