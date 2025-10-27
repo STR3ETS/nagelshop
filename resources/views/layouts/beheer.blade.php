5f1484fd64838a43c26d545cdda49aceed61711a
Boyd Halfman <78419518+STR3ETS@users.noreply.github.com>
2025-10-27 09:49:05 +0100

    Beter werkt t

--- resources/views/layouts/beheer.blade.php
+++ resources/views/layouts/beheer.blade.php
@@ -11,7 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @vite('resources/css/app.css', 'resources/js/app.js')
+        @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
         <script src="https://cdn.lordicon.com/lordicon.js"></script>
