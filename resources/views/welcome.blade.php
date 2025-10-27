036ce1bab7d468f0af46e2e80bf2eaa8eb86d0ed
Boyd Halfman <78419518+STR3ETS@users.noreply.github.com>
2025-10-27 23:40:53 +0100

    Filter show

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,10 @@
         <div class="absolute z-3 max-w-[1100px] px-[1rem] md:px-[3rem] left-0 right-0 ml-auto mr-auto">
             <h1 class="text-white text-[34px] md:text-[50px] font-bold leading-[1.15] pt-[100px]">Creëer <i class="instrument-serif-font">jouw droomnagels</i><br>met onze luxe producten</h1>
             <p class="text-white opacity-80 text-[15px] mt-4 mb-6">Ontdek hoogwaardige nagelproducten voor thuis of in de salon. Van gelpolish tot nail art.<br class="hidden md:block">Alles wat je nodig hebt om jouw droomnagels te creëren, vind je hier.</p>
-            <div class="p-4 rounded-xl bg-[#b38867]/50 border border-[#a0795c]/75 w-fit">
-                <h3 class="text-white text-sm font-medium tracking-tight">Nieuw jasje, zelfde topkwaliteit!</h3>
-                <p class="text-white opacity-80 text-xs">We stappen geleidelijk over op onze nieuwe verpakking.<br>Het product blijft precies hetzelfde alleen de look is vernieuwd.</p>
-            </div>
+        </div>
+        <div class="p-4 rounded-xl bg-[#b38867]/50 border border-[#a0795c]/75 w-fit absolute z-10 right-4 bottom-4">
+            <h3 class="text-white text-sm font-medium tracking-tight">Nieuw jasje, zelfde topkwaliteit!</h3>
+            <p class="text-white opacity-80 text-xs">We stappen geleidelijk over op onze nieuwe verpakking.<br>Het product blijft precies hetzelfde alleen de look is vernieuwd.</p>
         </div>
     </div>
 </div>
